6427695db122ee7c83557514f65b36a0deafd61e
Akshay Dhiman <akshay.virgo@gmail.com>
2018-11-13 19:53:50 +0530

    design fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// Hides "Read more" button of schedule items with less lines.
 window.addEventListener('DOMContentLoaded', function() {
   var truncTexts = document.getElementsByClassName('section-schedule-item__right--text');
   for (var i = 0; i < truncTexts.length; i++) {
@@ -8,7 +9,7 @@ window.addEventListener('DOMContentLoaded', function() {
   }
 })
 
-
+// Expand schedule details
 document.onclick = function(e) {
   if(e.target.className === 'read-more') {
     var trucTextEl = e.target.parentElement.getElementsByClassName('section-schedule-item__right--text')[0];
@@ -16,3 +17,27 @@ document.onclick = function(e) {
     e.target.style.display = 'none';
   }
 }
+
+/* var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+function myFunction(x) {
+  if (x.matches) {
+    document.body.style.backgroundColor = "yellow";
+  } else {
+    document.body.style.backgroundColor = "pink";
+  }
+}
+
+var x = window.matchMedia("(max-width: 700px)");
+myFunction(x);
+x.addListener(myFunction);
+ */
